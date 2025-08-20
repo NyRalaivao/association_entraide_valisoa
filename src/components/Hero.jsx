e21a32fa687c75b2c5aaea561efa38338fa0ebca
Ny <ralaivaonylalaina@gmail.com>
2025-08-20 22:32:05 +0300

    rajout de la fonctionnalité mailto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,12 @@ function Hero() {
                         <p className="text-lg">
                             Créer un avenir où la solidarité, l'éducation, la santé et l'environnement se rejoignent pour un Madagascar plus juste et durable.
                         </p>
-                        <a href="#don" className="inline-block mt-6 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold py-3 px-6 rounded-lg shadow transition duration-200">
-    Faire un don</a>
+                        <a
+                            href="mailto:contact@mutualaidfoundation.org?subject=Faire%20un%20don&body=Bonjour,%0D%0A%0D%0AJe souhaite faire un don pour soutenir vos actions.%0D%0A%0D%0AMerci."
+                            className="inline-block mt-6 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold py-3 px-6 rounded-lg shadow transition duration-200"
+                        >
+                            Faire un don
+                        </a>
                     </div>
                 </div>
             </section>
